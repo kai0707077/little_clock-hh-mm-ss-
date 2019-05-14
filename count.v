6f8ce624205fbf587d7ef67d50919c3b2ada0f20
Kai <41181286+kai0707077@users.noreply.github.com>
2019-05-14 11:53:01 +0800

    Update count.v

--- count.v
+++ count.v
@@ -13,22 +13,22 @@ always@ (posedge clk or posedge rst)
         else if(count==24'h235959)          //day circle reset
 				count<=24'h000000;
 				
-        else if(count[19:0]==20'h95959)    //min 00:59->01:00
-            count<=count+20'h6A6A7;   
+	else if(count[19:0]==20'h95959)    //9:59:59->10:00:00
+            count<=count+20'h6A6A7;	   //hex100000 - hex95959 = hex6A6A7
 		  
-        else if(count[15:0]==16'h5959)           //min 9->10
+        else if(count[15:0]==16'h5959)           
             count<=count+16'hA6A7;
 		  
-        else if(count[11:0]==12'h959)     //sec 00:59->01:00
+        else if(count[11:0]==12'h959)    
             count<=count+12'h6A7;
        
-        else if(count[7:0]==8'h59)            //sec 9->10
+        else if(count[7:0]==8'h59)          
             count<=count+8'hA7;
 		 
         else if(count[3:0]==4'h9)
-            count<=count+4'h7;             //sec+1
+            count<=count+4'h7;             
         
-		  else
+	else
             count<=count+1;
 	  end
 
