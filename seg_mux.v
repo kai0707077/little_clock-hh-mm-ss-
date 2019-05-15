2d3991c4a39685b155f6cfd2aceb834169080c05
Kai <41181286+kai0707077@users.noreply.github.com>
2019-05-15 11:18:59 +0800

    Update seg_mux.v

--- seg_mux.v
+++ seg_mux.v
@@ -22,26 +22,30 @@ module seg_mux(
 	
 		input wire [23:0]count,
 		input [3:0]sel,
-		
+		input rst,
+	
 		output reg [3:0]x
     );
 	 
 always@(sel)
-	case(sel)
+	if(rst==1)
+		x=0;
+	else begin
+		case(sel)
 //-----------------display hour & min-----------------------------------------//
-       //4'b1110:x=count[11:8];
-       //4'b1101:x=count[15:12];
-       //4'b1011:x=count[19:16];
-       //4'b0111:x=count[23:20];
+       		//4'b1110:x=count[11:8];
+       		//4'b1101:x=count[15:12];
+       		//4'b1011:x=count[19:16];
+       		//4'b0111:x=count[23:20];
 
 //-----------------display min & sec------------------------------------------//		 
-       4'b1110:x=count[3:0];
-       4'b1101:x=count[7:4];
-       4'b1011:x=count[11:8];
-       4'b0111:x=count[15:12];
+       		4'b1110:x=count[3:0];
+       		4'b1101:x=count[7:4];
+       		4'b1011:x=count[11:8];
+       		4'b0111:x=count[15:12];
 		 
 	endcase
-	 
+	end
 
 
 endmodule
